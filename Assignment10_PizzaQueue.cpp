9e8cd2c6851a6a8112f2816aec98fa6c2d1052bd
tejasson <144206036+tejasson@users.noreply.github.com>
2023-11-23 14:37:07 +0530

    Add files via upload

---
+++ Assignment10_PizzaQueue.cpp
@@ -0,0 +1,69 @@
+// Simulation of pizza parlor: 
+// Pizza parlor accepting maximum M orders.
+// Orders are served on a first come first served basis. Order once placed cannot be canceled.
+// Write C++ program to simulate the system using simple queue or circular queue
+
+#include<iostream>
+using namespace std;
+
+class PizzaParty{
+    int pizzaId = 0, front = 0, pizzaQueue[10];
+
+    public:
+        void pizzaOrder(){
+            if (pizzaId >= 10){
+                cout<<"cafe is full😟 come back later"<<endl;
+            }
+            else{
+                cout<<"Your order is placed 😁, your order no. is: "<<pizzaId+1<<endl;
+                pizzaQueue[pizzaId] = pizzaId+1;
+                pizzaId++;
+            }
+        }
+        void pizzaServe(){
+            if (pizzaId == 0||front > pizzaId){
+                cout<<"All orders have been served 😁"<<endl;
+            }
+            else {
+                cout<<endl<<"Order no. "<<front+1<<" is ready...😋"<<endl;
+                front++;
+            }
+        }
+        void display(){
+            cout<<"pending orders are: "<<endl;
+            for (int i = front; i <= pizzaId; i++){
+                cout<<pizzaQueue[i]<<",    ";
+            }
+        }
+};
+
+int main(){
+    PizzaParty obj;
+    int choice = 0;
+    while (choice != 4)
+    {
+        cout<<endl<<endl<<"1. Order Pizza"<<endl
+        <<"2. Serve Pizza"<<endl
+        <<"3. Display Orders"<<endl
+        <<"4. Exit"<<endl;
+        cout<<"your choice: "; cin>>choice;
+
+        switch (choice)
+        {
+        case 1:
+            obj.pizzaOrder();
+            break;
+        
+        case 2:
+            obj.pizzaServe();
+            break;
+        
+        case 3:
+            obj.display();
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
