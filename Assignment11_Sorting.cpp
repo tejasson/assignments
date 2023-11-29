8c66e63a71df010e7672028abf5f6aa92e6ac0bc
tejasson <144206036+tejasson@users.noreply.github.com>
2023-11-29 19:24:20 +0530

    Update Assignment11_Sorting.cpp

--- Assignment11_Sorting.cpp
+++ Assignment11_Sorting.cpp
@@ -1,3 +1,17 @@
+// Sorting: Write a C++ menu driven program to store the percentage of 
+// marks obtained by the students in an array. Write function for sorting array 
+// of floating point numbers in ascending order using 
+// 1. Selection Sort *
+// 2. Bubble sort *
+// 3. Insertion sort *
+// 4. Shell Sort 
+// 5. Quick sort *
+// 6. Radix sort 
+// 7. Display top five scores 
+// Implement 4 methods of sorting. Provide choice to user to take input from 
+// user or using random numbers. 
+// Use Standard Template Library (STL) sort function for above data.
+
 #include<iostream>
 using namespace std;
 
@@ -27,27 +41,32 @@ class Sorting
 
         void insertSort()
         {
-            for ( int i = 0; i < 5; i++)
+            for ( int i = 1; i < 5; i++)
             {
-                for (int j = i; j >= 0; j--)
+                for (int j = i-1; j >= 0; j--)
                 {
-                    if (arr[i] > arr[j])
+                    if (arr[i] < arr[j])
                     {
                         temp = arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;
+                        i--;
                     }
                 }
+            }
+            for (int i = 0; i < 5; i++)
+            {
                 cout<<arr[i]<<",";
             }
+            cout<<endl;
         }
 };
 
 int main()
 {
-    Sorting Obj;
-    Obj.bubbleSort();
-    Obj.insertSort();
+    Sorting obj;
+    obj.bubbleSort();
+    obj.insertSort();
 
     return 0;
 }
